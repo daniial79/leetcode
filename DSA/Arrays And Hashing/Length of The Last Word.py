ea825b735d8544b8ce7c868b2d5ca9211c3c964e
danial pezeshpoor <dpezeshpoor@yahoo.com>
2024-10-17 15:14:55 +0330

    Arrays And Hashing: Length of The Last Word optimized

--- DSA/Arrays And Hashing/Length of The Last Word.py
+++ DSA/Arrays And Hashing/Length of The Last Word.py
@@ -1,2 +1,19 @@
-def length_of_last_word(s: str) -> int:
+def length_of_last_word_v1(s: str) -> int:
     return len(s.split()[-1])
+
+
+
+def length_of_last_word_v2(s: str) -> int:
+    SPACE_ASCII = 32
+    length = 0
+    
+    i = -1
+    
+    # skipping all space characters from end of string
+    while s[i] != chr(SPACE_ASCII):
+        i -= 1
+        
+    # count length of last word
+    while s[i] != chr(SPACE_ASCII):
+        length += 1
+        i -= 1
